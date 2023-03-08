c0aa26be58f381ba08bb9e69341a5313124cbefc
sr4136 <4681620+sr4136@users.noreply.github.com>
2023-03-08 11:44:04 -0500

    Update igtestimport.php

--- igtestimport.php
+++ igtestimport.php
@@ -31,7 +31,7 @@ function igti_register_options_page_markup() {
 
 	<section id="igti_upload">
 		<h3>Uploaded input:</h3>
-		<textarea class="form-control" rows=5 cols=120 id="igti_input"></textarea>
+		<textarea class="form-control" rows=5 style="width:95%;" id="igti_input"></textarea>
 	</section>
 
 	<script>
@@ -46,28 +46,83 @@ function igti_register_options_page_markup() {
 			let reader = new FileReader();
 
 			// Closure to capture the file information.
-			reader.onload = (function(the_file) {
+			reader.onload = (function() {
 				return function(e) {
 					const the_result = e.target.result;
 					$('#igti_input').val(the_result);
-					console.log(the_result);
+					//console.log(the_result);
 
 					const the_json = JSON.parse(the_result);
-					console.log(the_json);
-					console.log(the_json[0]);
-					do_json_stuff();
+					//console.log(the_json);
+					do_json_stuff(the_json);
 				};
 			})(f);
 
 			// Read in the image file as a data URL.
-			reader.readAsText(the_file);
+			reader.readAsText(f);
 		}
 
-		function do_json_stuff() {
+		function do_json_stuff(the_json) {
+			// Make sure the API is ready.
 			wp.api.loadPromise.done(function() {
-				//... use the client here
-			})
-		}
+				const the_first = the_json[0];
+				console.table("Data:", the_first);
+
+				const taken_at = the_first.taken_at;
+				console.table("Date Raw: ", taken_at);
+
+				const the_date = new Date((taken_at * 1000));
+				console.table("Date Converted: ", the_date);
+
+				const the_caption = the_first.caption.text;
+				console.table("Caption: ", the_caption);
+
+				const the_images = the_first.carousel_media;
+				let the_images_urls = [];
+				the_images.forEach(function(img) {
+					the_images_urls.push(img.image_versions2.candidates[0].url);
+				});
+				//
+				console.table("Image URLS: ", the_images_urls);
+
+				// ❗❗❗❗ Waiting on the HTML IG export so I can have the imagessss.
+
+				let post_content = `
+					<!-- wp:gallery {"linkTo":"none"} -->
+					<figure class="wp-block-gallery has-nested-images columns-default is-cropped">
+				`;
+
+				post_content += `
+						<!-- wp:image {"id":35,"sizeSlug":"large","linkDestination":"none"} -->
+							<figure class="wp-block-image size-large"><img src="http://igtest.local/wp-content/uploads/2023/03/one.jpg" alt="" class="wp-image-35"/></figure>
+						<!-- /wp:image -->
+						<!-- wp:image {"id":36,"sizeSlug":"large","linkDestination":"none"} -->
+							<figure class="wp-block-image size-large"><img src="http://igtest.local/wp-content/uploads/2023/03/two.jpg" alt="" class="wp-image-36"/></figure>
+						<!-- /wp:image -->
+				`;
+
+				if (the_caption) {
+					post_content += `<figcaption class="blocks-gallery-caption wp-element-caption">${the_caption}</figcaption>`;
+				}
+
+				post_content += `
+					</figure>
+					<!-- /wp:gallery -->
+				`;
+
+
+				let post = new wp.api.models.Post({
+					title: 'IG Import - ' + taken_at,
+					status: 'publish',
+					content: post_content,
+					date: the_date
+				});
+				post.save().done(function(post) {
+					console.log('🟢 Post Created: "' + post.title.raw + '"');
+				});
+
+			}) // wp.api.loadPromise.done
+		} // do_json_stuff
 
 		document.getElementById('upload').addEventListener('change', handleFileSelect, false);
 	</script>
